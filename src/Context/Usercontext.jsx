3f5feaa1a41bba2f062be8acdb03d066f15a3a9f
YelveTejas <tejas.yelve20@gmail.com>
2024-03-12 03:52:13 +0530

    integrated with backend

---
+++ src/Context/Usercontext.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { createContext, useContext, useState } from "react";
+
+const UserContext = createContext();
+
+const Usercontext = ({ children }) => {
+  const [user, setUser] = useState();
+
+  return (
+    <UserContext.Provider value={{ setUser, user }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const UserState = () => {
+  return useContext(UserContext);
+};
+
+export default Usercontext;
